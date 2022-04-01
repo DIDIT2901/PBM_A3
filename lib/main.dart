f374507f41369082e2ab0f4fcf89e5fcde9d7b8e
Clarista Citra Marcellina <claristacitram@gmail.com>
2022-04-01 17:59:37 +0700

    coba get started

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
-//  const Myapp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SvgPicture.asset(
-          'assets/get_started.svg'
-        ),
+        body: Container(child: LoginPage()),
       ),
     );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 100),
+                child: Text(
+                  "Halo! Selibu Disini",
+                ),
+              ),
+              Text("Kenalan Dulu Yuk Sama Aplikasi Selibu!")
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 100),
+          child: Center(
+            child: Image.asset("assets/Buku.png"),
+          ),
+        ),
+        Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "Jadi, di aplikasi Selibu ini, Kamu Bisa Menyewa dan Membeli Buku loh! Baru Tau kan?",
+                ),
+              ),
+              Text("Temukan Buku Favoritmu Di Aplikasi Selibu")
+            ],
+          ),
+        ),
+        Container(
+            child: Container(
+                margin: EdgeInsets.all(32),
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(50)),
+                child: Center(
+                    child: Text("Lanjut Login",
+                        style: TextStyle(color: Colors.white, fontSize: 16)))))
+      ],
+    ));
   }
 }
