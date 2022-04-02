9d9195d0b623421cee6474c4206c77a2b098d055
Clarista Citra Marcellina <claristacitram@gmail.com>
2022-04-02 10:16:07 +0700

    background

--- lib/Screen/Welcome/components/body.dart
+++ lib/Screen/Welcome/components/body.dart
@@ -1,30 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:selibu/Screen/Welcome/components/background.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/Vektor_1.png",
-              width: size.width * 0.3,
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 100,
-            child: Image.asset(
-              "assets/images/Vektor_2.png",
-              width: size.width * 0.2,
-            ),
+          Text(
+            "Halo! Selibu Disini!",
+            style: TextStyle(color: Colors.black),
           ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Auth',
+      title: 'Selibu',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
