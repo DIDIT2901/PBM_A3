2237c495fd8e4f8a35923920fa211d20e19ff17d
laila1707 <81139316+laila1707@users.noreply.github.com>
2022-04-03 17:08:25 +0700

    Update signup_screen.dart

--- lib/Screen/SignUp/signup_screen.dart
+++ lib/Screen/SignUp/signup_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:selibu/Screens/Signup/components/body.dart';
+import 'package:selibu/Screen/Signup/components/body.dart';
 
 class SignUpScreen extends StatelessWidget {
   @override
