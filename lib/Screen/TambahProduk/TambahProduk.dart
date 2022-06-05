3a405659e72467472bb839844b64224087643874
Clarista Citra Marcellina <claristacitram@gmail.com>
2022-06-05 10:01:00 +0700

    add Tambah Produk

---
+++ lib/Screen/TambahProduk/TambahProduk.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:selibu/Screen/TambahProduk/component/body.dart';
+
+class TambahProduk extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
